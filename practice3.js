da4ea137b00d323976e72da6717907e0913b9f49
ray sonntag <saltydog727@gmail.com>
2021-12-13 11:01:39 -0500

    first commit

---
+++ practice3.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("server started on post 3000");
+})
